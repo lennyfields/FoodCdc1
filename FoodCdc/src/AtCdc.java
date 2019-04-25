2ea018134e7eb1fcc098e2f0d21276247780081e
lennyfields <leofield@seas.upenn.edu>
2019-04-25 06:02:03 -0400

    Add interactive map

---
+++ FoodCdc/src/AtCdc.java
@@ -0,0 +1,15 @@
+import java.awt.Desktop;
+import java.net.URI;
+
+
+public class AtCdc {
+
+	public static void main(String[] args) throws Exception
+	{
+		Desktop d = Desktop.getDesktop();
+		d.browse(new URI("https://arcg.is/mP941"));
+		
+
+	}
+
+}
